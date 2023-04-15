12913f5ff38b10423895e3c975fc93635bce9899
Anas <anastiour@gmail.com>
2023-04-15 02:56:12 +0000

    Read ABI from file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::fs::File;
+use ethereum_abi::Abi;
+
 fn main() {
-    println!("Hello, world!");
+    // Parse ABI JSON file
+    let abi = {
+        let file = File::open("./ABI.json").expect("failed to open ABI file");
+        Abi::from_reader(file).expect("failed to parse ABI")
+    };
 }
